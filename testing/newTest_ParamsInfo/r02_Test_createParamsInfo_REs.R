3bea2433aef61060c1a9569aaa43cce34782d824
William Stockhausen <william.stockhausen@noaa.gov>
2026-01-28 15:27:19 -0800

    2026.01.28. Just created new Github PAT.

--- testing/newTest_ParamsInfo/r02_Test_createParamsInfo_REs.R
+++ testing/newTest_ParamsInfo/r02_Test_createParamsInfo_REs.R
@@ -26,16 +26,19 @@ mfYXPx  = dmsYSC |> dplyr::filter(x=="male")   |> dplyr::select(sp_idx=sparse_id
 mfYXMf  = dmsYSC |> dplyr::filter(x=="female") |> dplyr::select(sp_idx=sparse_idx,y,x,m) |> dplyr::distinct() |>
             dplyr::mutate(yn=as.numeric(y));
 
-f_m = ~0 + (1|yblk) + ar1(y|p) + (1+y||p);
+##--some fake formulas to test code
+f_m = ~0 + (1|yblk) + ar1(y|p) + (1+y||p); #--resulting blist in reTerms has two [[p]] elements!
 f_f = ~0 + m + ar1(y|yblk);
 
+#--just test the REs creation here (need to finish this function; works to creating reTerms)
 pi_pLnA_m = createParamsInfo_REs(formula=f_m,
                                  model_frame=mfYXPx,
                                  contrasts=list(yblk=contr_none),
                                  sparse=TRUE);
-View(as.matrix(pi_pLnA_m$X));
-View(pi_pLnA_m$model_matrix);
+View(as.matrix(pi_pLnA_m$X)); #--don't think this works with new "blist"
+View(pi_pLnA_m$model_matrix); #--don't think this works with new "blist"
 
+#--just test the FEs creation here
 pi_pLnA_f = createParamsInfo_FEs(formula=f_f,
                                  model_frame=mfYXMf,
                                  contrasts=list(m=contr_none),
